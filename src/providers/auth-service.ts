833387eec8dffdfb8168b4e1171d6ad3f9f1c394
Chris Chen <cschen13@gmail.com>
2017-04-09 17:04:54 -0500

    Facebook auth should work on native apps now

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AuthProviders, AngularFireAuth, FirebaseAuthState, AuthMethods } from 'angularfire2';
+import { Platform } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from 'ionic-native';
+import firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
   private authState: FirebaseAuthState;
 
-  constructor(public auth$: AngularFireAuth) {
+  constructor(public auth$: AngularFireAuth, private platform: Platform) {
     this.authState = auth$.getAuth();
     auth$.subscribe((state: FirebaseAuthState) => {
       this.authState = state;
@@ -16,11 +19,18 @@ export class AuthService {
     return this.authState !== null;
   }
 
-  signInWithFacebook(): firebase.Promise<FirebaseAuthState> {
-    return this.auth$.login({
-      provider: AuthProviders.Facebook,
-      method: AuthMethods.Popup
-    });
+  signInWithFacebook(): firebase.Promise<FirebaseAuthState | FacebookLoginResponse> {
+    if (this.platform.is('cordova')) {
+      return Facebook.login(['email', 'public_profile']).then(res => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+        return firebase.auth().signInWithCredential(facebookCredential);
+      });
+    } else {
+      return this.auth$.login({
+        provider: AuthProviders.Facebook,
+        method: AuthMethods.Popup
+      });
+    }
   }
 
   signOut(): void {
