c9608e931715e64e1c3f3d67f4e738d574ebab2c
paep3nguin <paep3nguin@gmail.com>
2017-04-24 00:34:31 -0500

    Added reminder to enableProdMode

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from "@angular/core";
 
+// TODO: Don't forget to enable prodMode!
+// enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
