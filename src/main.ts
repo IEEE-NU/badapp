111454c3b16f04d4ff3d074d571d13de6b996ff1
paep3nguin <paep3nguin@gmail.com>
2017-04-24 23:54:28 -0500

    Prep for heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { enableProdMode } from "@angular/core";
 
-// TODO: Don't forget to enable prodMode!
-// enableProdMode();
+import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
+
+if (environment.production) {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
